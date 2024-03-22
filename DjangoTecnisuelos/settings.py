94ff815c5d9a0df61fe611edfeabed702e321d27
Sak-s <sanaveca@gmail.com>
2024-03-21 21:36:23 -0500

    Add Whitenoise to handle static files and improve security
    This commit adds Whitenoise to the DjangoTecnisuelos project to handle static files and improve security. The following changes were made:
    
    1. Added 'whitenoise.runserver_nostatic' to the INSTALLED_APPS setting.
    2. Added 'whitenoise.middleware.WhiteNoiseMiddleware' to the MIDDLEWARE setting, right after 'django.middleware.security.SecurityMiddleware'.
    3. Added 'whitenoise' to the requirements.txt file.
    
    These changes will enable Whitenoise to handle static files during development and production, and provide additional security by compressing, caching, and serving static files efficiently.

--- DjangoTecnisuelos/settings.py
+++ DjangoTecnisuelos/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'import_export',
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'whitenoise.runserver_nostatic',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -133,6 +134,7 @@ JAZZMIN_UI_TWEAKS = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
